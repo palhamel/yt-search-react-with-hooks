28795745e8a86e1fb542bd1cf032fb432d7c161b
Pål Hamel <pal.hamel@gmail.com>
2020-11-02 16:24:50 +0100

    move to new SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,42 +1,46 @@
 import React, { useState } from "react";
+
 // refactoring the SearchBar. Functional components.
-const SearchBar = () => {
- const [term, setTerm] = useState('');
-}
+
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState("");
+
+  const onInputChange = (event) => {
+    setTerm(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    onFormSubmit(term);
+  };
+  return (
+    <div className="search-bar ui purple segment">
+      <form onSubmit={onSubmit} className="ui form">
+        <div className="field ">
+          <label>
+            <h3> Search for Youtube videos </h3>
+          </label>
+          <div className="ui pointing below label purple">
+            Please enter something to search for on YouTube and hit Enter:
+          </div>
+          <div className="ui action left icon input">
+            <i className="search icon"></i>
+            <input
+              type="text"
+              placeholder="Cuddly Cows"
+              value={term}
+              onChange={onInputChange}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+//-----------
 
 export class SearchBar extends React.Component {
-
-  onInputChange = (event) => {
-    this.setState({ term: event.target.value });
-  };
-
-  onFormSubmit = (event) => {
-    event.preventDefault();
-    // console.log('from searchbar:', this.state.term);
-    this.props.onFormSubmit(this.state.term);
-  };
-
-  render() {
-    return (
-      <div className="search-bar ui purple segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field ">
-            <label>
-              <h3> Search for Youtube videos </h3>
-            </label>
-            <div className="ui pointing below label purple">Please enter something to search for on YouTube and hit Enter:</div>
-            <div className="ui action left icon input">
-              <i className="search icon"></i>
-              <input
-                type="text"
-                placeholder="Cuddly Cows"
-                value={this.state.term}
-                onChange={this.onInputChange}
-              />
-            </div>
-          </div>
-        </form>
-      </div>
-    );
-  }
+  render() {}
 }
