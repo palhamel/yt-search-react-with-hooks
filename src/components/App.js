e02ee2b35c2250c140041cfcab7e1f06157b523d
Pål Hamel <pal.hamel@gmail.com>
2020-11-04 19:36:34 +0100

    refactoring the JSX part

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import { VideoDetail } from "./VideoDetail";
 
 // useState x 2
 
-const App = () => {
+export const App = () => {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
 
@@ -29,30 +29,20 @@ const App = () => {
   const onVideoSelect = (video) => {
     setSelectedVideo(video);
   };
-};
-
-// TODO: --------- Klar ovan - kvar under  - JSX block
-
-export class App extends React.Component {
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <p className="ui purple right ribbon label">Videos:</p>
-              <VideoList
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos}
-              />
-            </div>
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={onTermSubmit} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <p className="ui purple right ribbon label">Videos:</p>
+            <VideoList onVideoSelect={onVideoSelect} videos={videos} />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
